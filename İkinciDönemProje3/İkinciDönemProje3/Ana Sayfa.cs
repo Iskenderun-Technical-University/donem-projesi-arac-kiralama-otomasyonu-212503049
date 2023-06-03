5a007b234518fdbbf6aa7850c2ed7086033739eb
emiresenn <emireseb@gmail.com>
2023-06-03 18:19:08 +0300

    Proje

---
+++ İkinciDönemProje3/İkinciDönemProje3/Ana Sayfa.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İkinciDönemProje3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || textBox3.Text == "")
+
+                MessageBox.Show("Kullanıcı adı ve/veya şifre giriniz.", "Uyarı");
+
+
+            else
+            {
+
+                if (textBox2.Text == "1" && textBox3.Text == "1")
+                {
+
+                    Form2 frm = new Form2();
+                    frm.Show();
+                    this.Hide();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı ve/veya şifre hatalı.", "Uyarı");
+
+
+                }
+
+
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+    }
+}
